0162df6992128ea31904316be614698857999904
Yisuer <3310821161@qq.com>
2021-09-16 16:15:24 +0800

    add wave generator and fix

--- mapping.py
+++ mapping.py
@@ -5,14 +5,15 @@
 import waveform
 import math
 
-test_skeleton = [[0, 0, 0], [197.164, 161.491, 0.896455], [237.798, 158.156, 0.840285], [259.772, 124.262, 0.871383],
-                 [253.03, 68.2592, 0.912764], [159.813, 161.534, 0.834573], [141.215, 125.88, 0.902222],
-                 [147.987, 64.9346, 0.871515], [197.162, 288.63, 0.71238], [222.506, 288.636, 0.686935],
-                 [212.342, 386.976, 0.880746], [205.54, 491.988, 0.811595], [173.407, 288.634, 0.675585],
-                 [186.92, 388.62, 0.857565], [183.509, 486.939, 0.829572], [0, 0, 0], [0, 0, 0],
-                 [220.871, 122.566, 0.908147], [178.456, 119.106, 0.884239], [183.535, 507.266, 0.575368],
-                 [178.456, 505.561, 0.614999], [181.896, 497.058, 0.770747], [212.357, 514.067, 0.613764],
-                 [212.334, 512.348, 0.639208], [203.833, 500.513, 0.782646]]
+test_skeleton = [7,
+                 [[0, 0, 0], [197.164, 161.491, 0.896455], [237.798, 158.156, 0.840285], [259.772, 124.262, 0.871383],
+                  [253.03, 68.2592, 0.912764], [159.813, 161.534, 0.834573], [141.215, 125.88, 0.902222],
+                  [147.987, 64.9346, 0.871515], [197.162, 288.63, 0.71238], [222.506, 288.636, 0.686935],
+                  [212.342, 386.976, 0.880746], [205.54, 491.988, 0.811595], [173.407, 288.634, 0.675585],
+                  [186.92, 388.62, 0.857565], [183.509, 486.939, 0.829572], [0, 0, 0], [0, 0, 0],
+                  [220.871, 122.566, 0.908147], [178.456, 119.106, 0.884239], [183.535, 507.266, 0.575368],
+                  [178.456, 505.561, 0.614999], [181.896, 497.058, 0.770747], [212.357, 514.067, 0.613764],
+                  [212.334, 512.348, 0.639208], [203.833, 500.513, 0.782646]]]
 
 
 # def test_mapping(skeleton):
@@ -58,45 +59,45 @@ def point_distance(a, b):
 # simplest version mappings
 def push_up_mapping(skeleton):
     if True:
-        heel = midpoint([skeleton[21], skeleton[24]])
-        h = heel[1] - skeleton[1][1]
-        l = point_distance(heel, skeleton[1])
-        i = h / l
+        heel = midpoint([skeleton[1][21], skeleton[1][24]])
+        h = heel[1] - skeleton[1][1][1]
+        k = point_distance(heel, skeleton[1][1])
+        i = h / k
         if i <= 0:
-            return 0
+            return [skeleton[0], 0]
         elif i >= 0.35:
-            return 1
+            return [skeleton[0], 1]
         else:
-            return i / 0.35
+            return [skeleton[0], i / 0.35]
     return -1
 
 
 def sit_up_mapping(skeleton):
-    h = skeleton[8][1] - skeleton[1][1]
-    l = point_distance(skeleton[8], skeleton[1])
-    i = h / l
+    h = skeleton[1][8][1] - skeleton[1][1][1]
+    k = point_distance(skeleton[1][8], skeleton[1][1])
+    i = h / k
     if i <= 0:
-        return 0
+        return [skeleton[0], 0]
     elif i >= 0.9:
-        return 1
+        return [skeleton[0], 1]
     else:
-        return i
+        return [skeleton[0], i]
 
 
 def pull_up_mapping(skeleton):
-    i = abs(skeleton[4][0] - skeleton[7][0])
+    i = abs(skeleton[1][4][0] - skeleton[1][7][0])
     # check the distance between two wrist is less than shoulder width
-    if abs(skeleton[2][0] - skeleton[5][0]) * 1.5 >= i:
-        p = skeleton[1][1] - midpoint([skeleton[4], skeleton[7]])[1]
+    if abs(skeleton[1][2][0] - skeleton[1][5][0]) * 1.5 >= i:
+        p = skeleton[1][1][1] - midpoint([skeleton[1][4], skeleton[1][7]])[1]
         if p >= i * 1.5:
             print('Initial position.')
-            return 0
+            return [skeleton[0], 0]
         elif p <= 0:
             print('Pulled up.')
-            return 1
+            return [skeleton[0], 1]
         else:
             print(1 - p / i / 1.5)
-            return 1 - p / i / 1.5
+            return [skeleton[0], 1 - p / i / 1.5]
     print('Wrist distance too large.')
     return -1
 
--- wave_generator.py
+++ wave_generator.py
@@ -1,40 +1,37 @@
-# import mapping
-# import utils
-#
-# # deciding whether the action has begun
-# t = 0.1
-# # threshold for judging whether the action is valid
-# T = 0.15
-# sample = None
-#
-#
-# def generate(video):
-#     r = 0
-#     f = []
-#     flag = [False, -1]
-#     # TODO:make sure openpose python works
-#     skeleton_data = utils.skeleton_extraction(video)
-#     for item in skeleton_data:
-#         # TODO:map result including frame number info
-#         y = mapping.test_mapping(item)
-#         if y is not -1:
-#             deviation = abs(y[1])
-#             if flag[0] is False and deviation < t:
-#                 flag[1] = y[0]
-#             elif flag[0] is False and deviation >= t:
-#                 flag[0] = True
-#                 f.append(y)
-#             elif flag is True and deviation >= t:
-#                 f.append(y)
-#             elif flag is True and deviation < t:
-#                 f.append(y)
-#                 # using a simple 1D average error judging
-#                 if judging.ave_error_judging1d(flag[1], f, judging.sin) >= T:
-#                     r += 1
-#                 f = []
-#                 flag = [False, -1]
-#     print(r)
-#
-#
-# if __name__ == '__main__':
-#     counting(sample)
+import mapping
+import utils
+
+# deciding whether the action has begun
+t = 0.1
+# threshold for judging whether the action is valid
+sample = None
+mapper = mapping.pull_up_mapping
+
+
+def generate(videos):
+    for video in videos:
+        f = []
+        p = []
+        flag = [False, -1]
+        skeleton_data = utils.skeleton_extraction(video)
+        for item in skeleton_data:
+            y = mapper(item)
+            if y is not -1:
+                deviation = abs(y[1])
+                if flag[0] is False and deviation < t:
+                    flag[1] = y[0]
+                elif flag[0] is False and deviation >= t:
+                    flag[0] = True
+                    f.append(y)
+                elif flag is True and deviation >= t:
+                    f.append(y)
+                elif flag is True and deviation < t:
+                    f.append(y)
+                    p += utils.frame_regularization(f)
+                    f = []
+                    flag = [False, -1]
+    utils.simple_fitting(p)
+
+
+if __name__ == '__main__':
+    generate(sample)
